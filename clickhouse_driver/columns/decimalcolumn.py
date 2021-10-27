7be13459bb9874cea91843938b04a81d06e4d149
Robin <zsmeijin@hotmail.com>
2021-10-27 14:44:25 +0800

    decimal column 在 largeint.pyx 中需要进行位运算，因此必须保证 item 类型为 int

--- clickhouse_driver/columns/decimalcolumn.py
+++ clickhouse_driver/columns/decimalcolumn.py
@@ -1,5 +1,7 @@
 from decimal import Decimal, localcontext
 
+import numpy as np
+
 from .base import FormatColumn
 from .exceptions import ColumnTypeMismatchException
 from .intcolumn import Int128Column, Int256Column
@@ -62,6 +64,43 @@ class DecimalColumn(FormatColumn):
                     items[i] = null_value
                 else:
                     items[i] = int(Decimal(str(item)))
+
+    def prepare_items(self, items):
+        nullable = self.nullable
+        null_value = self.null_value
+
+        check_item = self.check_item
+        if self.types_check_enabled:
+            check_item_type = self.check_item_type
+        else:
+            check_item_type = False
+
+        if (not self.nullable and not check_item_type and
+                not check_item and not self.before_write_items):
+            return items
+
+        nulls_map = [False] * len(items) if self.nullable else None
+        for i, x in enumerate(items):
+            if x is None and nullable:
+                nulls_map[i] = True
+                x = null_value
+
+            else:
+                if check_item_type:
+                    check_item_type(x)
+
+                if check_item:
+                    check_item(x)
+
+            items[i] = x
+
+        if self.before_write_items:
+            self.before_write_items(items, nulls_map=nulls_map)
+            if type(items) == np.ndarray:
+                # 在 largeint.pyx 中需要进行位运算，因此必须保证 item 类型为 int
+                return items.astype(np.int64, copy=False).tolist()
+
+        return items
 
     # Override default precision to the maximum supported by underlying type.
     def _write_data(self, items, buf):
